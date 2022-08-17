21223fca1bd23ca65ff5f438776b9b07fa149b98
Emily Medhurst <emily.medhurst2002@protonmail.com>
2022-08-18 00:30:16 +0100

    seo stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,25 @@
   import BodyLink from '$lib/BodyLink.svelte';
   import Portfolio from '$lib/sections/portfolio/Portfolio.svelte';
   import Experience from '$lib/sections/experience/Experience.svelte';
+
+  const title = 'Emily Medhurst';
+  const description = 'A trans software developer currently based in Portsmouth'
 </script>
 
 <svelte:head>
-  <title>Emily Medhurst</title>
-  <meta name="description" content="A trans software developer currently based in Portsmouth">
+  <title>{title}</title>
+  <meta name="description" content={description}>
+  <meta name="robots" content="index, follow">
+  <meta name="language" content="English">
+  <meta name="og:title" content={title}>
+  <meta name="og:description" content={description}>
+  <meta name="og:type" content="website">
+  <meta name="og:url" content="https://emilymedhurst.me/">
+  <meta itemprop="name" content={title}>
+  <meta itemprop="description" content={description}>
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content={title}>
+  <meta name="twitter:description" content={description}>
 </svelte:head>
 
 <main class='pt-4 text-xl flex flex-col gap-4'>
