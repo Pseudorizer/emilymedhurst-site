e403ebd1ff31dc343af42adcfa11ba24abf60f78
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-03-18 12:25:32 +0000

    add mkcert for local

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { resolve } from 'path';
+import mkcert from'vite-plugin-mkcert'
 
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), mkcert()],
   resolve: {
     alias: {
       $lib: resolve('./src/lib')
     }
   },
-  css: { devSourcemap: true }
+  css: { devSourcemap: true },
+  server: {
+    https: true,
+  }
 };
 
 export default config;
