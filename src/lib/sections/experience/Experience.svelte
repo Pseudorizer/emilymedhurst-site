4ca7c2ed5d4f4c1a3e0fdc0d515251bc66e87c28
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-03-20 22:53:42 +0000

    add radweb link to experience block

--- src/lib/sections/experience/Experience.svelte
+++ src/lib/sections/experience/Experience.svelte
@@ -2,6 +2,7 @@
   import Section from '$lib/Section.svelte';
   import Block from '$lib/sections/Block.svelte';
   import { IconBrandLaravel, IconBrandMysql, IconBrandPhp, IconBrandReact, IconBrandRedux, IconPoo } from '@tabler/icons-svelte';
+  import trackingEnabled from '$lib/stores/trackingStore';
 
   const reactCrumb = {
     icon: IconBrandReact,
@@ -32,11 +33,15 @@
     icon: IconPoo,
     text: 'flow'
   };
+
+  $: links = {
+    radweb: $trackingEnabled ? 'https://dub.sh/OfrKsKi' : 'https://radweb.com/',
+  }
 </script>
 
 <Section title='Experience'>
   <div class='grid grid-cols-1'>
     <Block title='Radweb' description='This was my first hire! I&#39;m currently working as a junior developer on InventoryBase. I&#39;ll write more about this in time.' subheading='2022-Current'
-           tags={[reactCrumb, phpCrumb, laravelCrumb, mysqlCrumb, reduxCrumb, flowCrumb, 'real customers']} />
+           tags={[reactCrumb, phpCrumb, laravelCrumb, mysqlCrumb, reduxCrumb, flowCrumb, 'real customers']} link={links.radweb} />
   </div>
 </Section>
