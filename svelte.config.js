45a4e6d04acdd6a1935150bb55ca1584acf1a2c9
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-04-02 17:31:00 +0100

    add csp

--- svelte.config.js
+++ svelte.config.js
@@ -4,16 +4,26 @@ import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.md', '.svx'],
-  preprocess: [
-    mdsvex({ extensions: ['.md', '.svx'] }),
-    preprocess({
-      postcss: true,
-    })],
-  kit: {
-    adapter: vercel(),
-    prerender: {entries: ["*"],},
-  },
+	extensions: ['.svelte', '.md', '.svx'],
+	preprocess: [
+		mdsvex({ extensions: ['.md', '.svx'] }),
+		preprocess({
+			postcss: true
+		})
+	],
+	kit: {
+		adapter: vercel(),
+		prerender: {entries: ["*"],  },
+		csp: {
+			directives: {
+				'script-src': ['self', 'unsafe-inline'],
+				'frame-src': ['self', 'https://restreamer.pseudorizer.com/'],
+				'object-src': ['none'],
+				'base-uri': ['self'],
+				'require-trusted-types-for': ['script']
+			}
+		}
+	}
 };
 
 export default config;
