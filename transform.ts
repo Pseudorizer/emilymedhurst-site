534aa2222c9b86e460ffa67d50d58e2203d492c7
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-04-14 13:13:49 +0100

    up deps

--- transform.ts
+++ transform.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import glob from 'glob';
+import { glob } from 'glob';
 import type { EntriesTransformFunction } from 'vite-plugin-svelte-entries-generator';
 
 const CONTENT_PATH = 'src/content/posts/*.svx';
