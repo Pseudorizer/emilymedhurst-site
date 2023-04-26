8af4d4751cbde3c9db7bf9520ecbabd062571e63
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-04-26 22:03:00 +0100

    remove twitter domain tag???

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,6 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="https://emilymedhurst.me/me.webp" />
 	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:domain" content="emilymedhurst.me" />
 	<meta property="twitter:image" content="https://emilymedhurst.me/me.webp" />
 </svelte:head>
 
