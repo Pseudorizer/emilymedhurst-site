8553decb8bd19e4c2680d5d4ffdda6185747f7ed
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-05-01 11:51:17 +0100

    shouldn't have deleted this

--- src/lib/Layout.svelte
+++ src/lib/Layout.svelte
@@ -5,8 +5,9 @@
     import h2 from '$lib/layoutComponents/H2.svelte';
     import h3 from '$lib/layoutComponents/H3.svelte';
     import img from '$lib/layoutComponents/Img.svelte';
+    import code from '$lib/layoutComponents/Code.svelte';
 
-    export { a, p, h1, h2, img, h3 }
+    export { a, p, h1, h2, img, h3, code }
 </script>
 
 <!-- Can't override pre -->
