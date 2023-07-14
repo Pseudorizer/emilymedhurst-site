1f2832064ee8e48e87d66356486c05ba602eafe0
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-07-14 17:24:31 +0100

    embed mastodon link in header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,7 @@
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:domain" content="emilymedhurst.me" />
 	<meta property="twitter:image" content="https://emilymedhurst.me/me.webp" />
+	<link href="https://tech.lgbt/@pseudorizer" rel="me">
 </svelte:head>
 
 <div class="mx-auto h-full max-w-4xl p-6 text-dark dark:text-light">
