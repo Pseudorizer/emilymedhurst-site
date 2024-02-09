6419d35240bdd31cdca94570e932a1a31ab84931
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-02-09 18:50:12 +0000

    put analytics inject into +layout.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script>
 	import '$src/app.css';
 	import Navbar from '$lib/navbar/Navbar.svelte';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-
-	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,8 @@
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
 
 export const prerender = true;
 
+inject({ mode: dev ? 'development' : 'production' });
 injectSpeedInsights();
