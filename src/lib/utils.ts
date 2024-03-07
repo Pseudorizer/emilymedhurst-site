6901947cc10e38502e8b1cbb70497c138c8dd96b
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-03-07 20:40:31 +0000

    improve don't show hidden articles

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import type { JSX } from 'astro/jsx-runtime';
 import type { Tag } from './types';
+import type { CollectionEntry } from 'astro:content';
 
 export const isSameSite = (href: string) => !href.startsWith('http');
 
@@ -8,3 +9,6 @@ export const createCrumb = (icon: JSX.Element, text: string) =>
 		text,
 		icon
 	}) satisfies Tag;
+
+export const isHidden = (article: CollectionEntry<'articles'>) =>
+	article.data.hidden && !import.meta.env.DEV;
